db7bd5785c7213842cb49c96141e21d1ca08631a
Victor <victorlopezruiz@eluig.xeill.net>
2026-05-07 19:05:18 +0200

    tiempo de 3 segundos entre pachanga y resultado creado

--- InazumaLegacy/src/main/java/net/elpuig/inazumalegacy/InazumaLegacyApplication.java
+++ InazumaLegacy/src/main/java/net/elpuig/inazumalegacy/InazumaLegacyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InazumaLegacyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InazumaLegacyApplication.class, args);
 	}
