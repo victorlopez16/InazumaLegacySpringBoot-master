44bdc9c235d5cd634e4cf8cef016478f35f07afc
Victor <victorlopezruiz@eluig.xeill.net>
2026-05-18 11:14:53 +0200

    "Fix: Force IPv4 for OpenAI connection

--- InazumaLegacy/src/main/java/net/elpuig/inazumalegacy/InazumaLegacyApplication.java
+++ InazumaLegacy/src/main/java/net/elpuig/inazumalegacy/InazumaLegacyApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InazumaLegacyApplication {
+
 	public static void main(String[] args) {
+		System.setProperty("java.net.preferIPv4Stack" , "true");
+
 		SpringApplication.run(InazumaLegacyApplication.class, args);
 	}
 
